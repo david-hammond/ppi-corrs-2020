7b2cc496f536d789809dc0ffdc91dc1246447442
taliahagerty <thagerty@economicsandpeace.org>
2020-04-28 16:26:31 +1000

    acceptable document

--- src/eda.R
+++ src/eda.R
@@ -1,4 +1,5 @@
 
 library('ProjectTemplate')
-load.project(override.config = list(data_loading =F))
+load.project(override.config = list(data_loading =T))
 
+render("notebook.Rmd", output_format = "pdf_document", output_file = "S:/Institute for Economics and Peace/Research/Research Briefs/Positive Peace/Peace group correlation analysis.pdf")
