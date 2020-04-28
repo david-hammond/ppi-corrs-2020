f87f08937bb4f53a75c6383a42b16c1948530cf7
taliahagerty <thagerty@economicsandpeace.org>
2020-04-28 17:16:48 +1000

    added 8 plots to doc

--- lib/plot by pillar.R
+++ lib/plot by pillar.R
@@ -9,7 +9,13 @@ plot_by_pillar <- function(x) {
   pp$group = gsub("cor", "Rank ", pp$group)
   
   p <- ggplot(pp, aes(x = group, y = variablename1, size = abs(r))) +
-    geom_point() + theme_minimal() + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+    geom_point() + theme_minimal() + theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    theme(axis.text.y = element_text(size = 7),
+          axis.text.x = element_text(size = 7),
+          legend.text = element_text(size = 7),
+          axis.title.x=element_blank(),
+          axis.title.y=element_blank()) +
+    labs(y = "", x = "")
   
   p 
   
