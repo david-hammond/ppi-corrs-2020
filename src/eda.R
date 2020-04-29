84242559ee76cae708aa2d7f8a09d7b4cdfd4aec
taliahagerty <thagerty@economicsandpeace.org>
2020-04-29 14:43:36 +1000

    switched output to word document

--- src/eda.R
+++ src/eda.R
@@ -2,4 +2,4 @@
 library('ProjectTemplate')
 load.project(override.config = list(data_loading =T))
 
-render("notebook.Rmd", output_format = "pdf_document", output_file = "S:/Institute for Economics and Peace/Research/Research Briefs/Positive Peace/Peace group correlation analysis.pdf")
+rmarkdown::render("notebook.Rmd", output_format = "word_document", output_file = "S:/Institute for Economics and Peace/Research/Research Briefs/Positive Peace/Peace group correlation analysis.doc")
